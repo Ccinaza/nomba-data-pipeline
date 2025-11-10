851c1e3a15706081d43c2c1e8dda23909e8063f2
Blessing Angus <blessing.angus@alerzo.com>
2025-11-10 09:31:00 +0100

    Update cdc strategy

--- dbt_project/nomba_dbt/models/staging/stg_users.sql
+++ dbt_project/nomba_dbt/models/staging/stg_users.sql
@@ -1,7 +1,7 @@
 {{ config(
     materialized='table', 
     engine='MergeTree',
-    order_by=['snapshot_date', 'user_id']
+    order_by=['user_id']
 ) }}
 
 select
@@ -10,13 +10,6 @@ select
     firstName as first_name,
     lastName as last_name,
     occupation,
-    state,
-    snapshot_date
+    state
 from {{ source('nomba', 'raw_users') }}
-
--- -- This block only runs when table exists AND it's not a full refresh
--- {% if is_incremental() %}
---   -- Only include rows where updated_at is newer than the max in the target table
---   where snapshot_date >= (select max(snapshot_date) from {{ this }})
--- {% endif %}
 
